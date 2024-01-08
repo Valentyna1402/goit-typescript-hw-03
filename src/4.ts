a569aca0320f9473fee6d2ebd9648ae9226efd80
Valentyna1402 <vriabchun@gmail.com>
2024-01-08 17:27:36 +0100

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,54 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {
+    this.signature = signature;
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome!");
+    } else {
+      console.log("The door is closed, you need a key to enter.");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open, you can enter.");
+    } else {
+      console.log("The key does not match. Door closed");
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
